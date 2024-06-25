253e5491f2176c4789d3f37dab06f745470bdbf7
Marcin Armacki <marcin.armacki@gmail.com>
2024-06-25 23:00:43 +0200

    Add linting script

---
+++ lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'RUNNING AUTOFLAKE...'
+autoflake --verbose --recursive --expand-star-imports --remove-all-unused-imports --in-place .
+
+echo -e '\nRUNNING BANDIT...'
+bandit -r --config pyproject.toml .
+
+echo -e '\nRUNNING BLACK...'
+black --config pyproject.toml .
+
+echo -e '\nRUNNING ISORT...'
+isort --settings-path pyproject.toml .
+
+echo -e '\nRUNNING MYPY...'
+mypy .
