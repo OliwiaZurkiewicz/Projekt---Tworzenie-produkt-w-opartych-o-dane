7e97e8465d8bac9395a5186dfe6f7568d246bd26
Marcin Armacki <marcin.armacki@gmail.com>
2024-07-16 20:19:28 +0200

    Final stretch for linting

--- lint.sh
+++ lint.sh
@@ -6,11 +6,11 @@ autoflake --verbose --recursive --expand-star-imports --remove-all-unused-import
 echo -e '\nRUNNING BANDIT...'
 bandit -r --config pyproject.toml .
 
-echo -e '\nRUNNING BLACK...'
-black --config pyproject.toml .
-
 echo -e '\nRUNNING ISORT...'
 isort --settings-path pyproject.toml --profile black .
+
+echo -e '\nRUNNING BLACK...'
+black --config pyproject.toml .
 
 echo -e '\nRUNNING MYPY...'
 mypy .
